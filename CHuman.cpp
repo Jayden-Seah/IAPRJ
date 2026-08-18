afc06b65bf4e030a76f54b891c582fc3ef7d3043
Ayraxyn <cyvellyeous@gmail.com>
2026-08-18 10:15:07 +0800

    Add files via upload

--- CHuman.cpp
+++ CHuman.cpp
@@ -1,1 +1,47 @@
 #include "CHuman.h"
+
+CHuman::CHuman()
+{
+    DetectionRange = 0;
+    isEntityFollowingPlayer = false;
+    isEntityRunningFromPlayer = false;
+    isEntityShooter = false; 
+    isEntityFreeToRoam = false;
+}
+
+bool CHuman::getFollowStatus()
+{
+    return isEntityFollowingPlayer;
+}
+
+bool CHuman::getRoamStatus()
+{
+    return isEntityFreeToRoam;
+}
+
+bool CHuman::getRunningAwayStatus()
+{
+    return isEntityRunningFromPlayer;
+}
+
+void CHuman::setFollowStatus(bool a)
+{
+    isEntityFollowingPlayer = a;
+}
+
+void CHuman::setRoamStatus(bool a)
+{
+    isEntityFreeToRoam = a;
+}
+
+void CHuman::setRunningAwayStatus(bool a)
+{
+    isEntityRunningFromPlayer = a;
+}
+
+void CHuman::setShooterStatus(bool a)
+{
+    isEntityShooter = a;
+}
+
+
--- CSolidHitbox.cpp
+++ CSolidHitbox.cpp
@@ -1,1 +1,11 @@
 #include "CSolidHitbox.h"
+
+bool CSolidHitbox::isEntityOverlapping(CSolidHitbox(*target)) // overlapping = true, not overlapping (good) = false
+{
+	for (int i = 0; i < getObjectHeight(); i++) { // 
+		if ((getCoordX() == target->getCoordX()) and (getCoordY()+i == target->getCoordY())) {
+			return false;
+		}
+	}
+	return true;
+}

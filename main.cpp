d60414b83bb74e13fe740586862a3e22ca2d6d34
Jayden Seah <jdseahgming2409@gmail.com>
2026-08-18 19:43:20 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -56,31 +56,33 @@ int main() {
 		}
 	}
 
-	// using string so we can add color with escape codes
-	std::string playBoardWidth[120][17];
+	int rows = 120;
+	int cols = 17;
+
+	std::vector<std::vector<std::string>> board(rows, std::vector<std::string>(cols, { ' ' }));
 	// PLAYING area for fighting board is only 104 x 11
 	for (int o = 0; o < 17; o++) {
 		for (int i = 0;i < 120; i++) {
 			if (o == 0 or o== 16) {
-				playBoardWidth[i][o] = "-";
+				board[i][o] = "-";
 			}
 			else if (o == 11){
 				if (i == 0 or i == 104 or i == 119) {
-					playBoardWidth[i][o] = "|";
+					board[i][o] = "|";
 				}
 				else if (i > 104){
-					playBoardWidth[i][o] = " ";
+					board[i][o] = " ";
 				}
 				else {
-					playBoardWidth[i][o] = "-";
+					board[i][o] = "-";
 				}
 			}
 			else {
 				if (i==0 or i==104 or i==119) {
-					playBoardWidth[i][o] = "|";
+					board[i][o] = "|";
 				}
 				else {
-					playBoardWidth[i][o] = " ";
+					board[i][o] = " ";
 				}
 			}
 		}
@@ -93,32 +95,32 @@ int main() {
 		}
 		switch (kl) {
 		case 1:
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[92mA\033[0m";
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[92mO\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[92mA\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[92mO\033[0m";
 			break;
 		case 2:
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[92mA\033[0m";
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[92mO\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[92mA\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[92mO\033[0m";
 			break;
 		case 3:
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[31mA\033[0m";
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[31mO\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[31mA\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[31mO\033[0m";
 			break;
 		case 4:
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[31mA\033[0m";
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[31mO\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[31mA\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[31mO\033[0m";
 			break;
 		case 5:
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[91mA\033[0m";
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[91mO\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[91mA\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[91mO\033[0m";
 			break;
 		case 6:
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[91mA\033[0m";
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[91mO\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[91mA\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[91mO\033[0m";
 			break;
 		case 7:
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[47m\033[30mA\033[0m";
-			playBoardWidth[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[47m\033[30mO\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY()] = "\033[47m\033[30mA\033[0m";
+			board[Human[i]->getCoordX()][Human[i]->getCoordY() - 1] = "\033[47m\033[30mO\033[0m";
 			break;
 		}
 	}
@@ -126,7 +128,7 @@ int main() {
 
 	for (int o = 0; o < 17; o++) {
 		for (int i = 0; i < 120; i++) {
-			std::cout << playBoardWidth[i][o];
+			std::cout << board[i][o];
 		}
 		std::cout << std::endl;
 	}

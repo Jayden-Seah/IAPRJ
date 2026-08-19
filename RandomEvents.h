80ae740abf8e8e1165bc54cb6007929f216b415b
Jayden Seah <jdseahgming2409@gmail.com>
2026-08-19 09:55:38 +0800

    Add files via upload

--- RandomEvents.h
+++ RandomEvents.h
@@ -1,14 +1,17 @@
 #pragma once
 #include <iostream>
+#include <random>
 
-srand(static_cast<unsigned>(time(0)));
+//std::default_random_engine generator(std::random_device{}());//default random engine creates the random value, generator is just the name for it , std random device gives it a random starting point without it itll be the same evry time
+
+//std::uniform_int_distribution<int> revents(0, 100);// uniform gives every num a equal chance, int is whole num, and distribution effectively says, give me a num from x to y
+
 
 class RandomEvents
 {
 public:
 	RandomEvents() {
-		int eventnum = (rand() % 5);
-		std::cout << eventnum;
+		//int eventnum = (revents(generator) % 5);
+		//std::cout << eventnum;
 	}
 };
-

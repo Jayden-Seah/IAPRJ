f17c43328e1359f77a422d2e70ebd8609a3504aa
Cank <sunny66ang@gmail.com>
2026-08-24 09:01:33 +0800

    Add files via upload

--- Upgrade.h
+++ Upgrade.h
@@ -1,10 +1,13 @@
 #pragma once
+#include <random>
 #include "CEntity.h"
 #include "CPlayer.h"
 class Upgrade :public CPlayer
 {
     static int killcount;
 
+    std::default_random_engine gene{std::random_device{}()};
+    std::uniform_int_distribution<int> event_dist{5,10};
     bool upg1;
 
     bool upg2;
@@ -13,5 +16,7 @@ public:
     void upgrade();
 
     void unupgrade();
-}
+
+
+};
 
